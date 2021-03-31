6e4c00fe945f5a3ba3a4e0c655cf187ff1acc0fa
Bernardo Español <esp.bernardo@gmail.com>
2021-03-30 21:45:25 -0300

    Calculo de la estabilidad de la ecuacion de Mathieu

---
+++ Mathieu_equations/Codigos/mathieu_equation_stability_bool.py
@@ -0,0 +1,51 @@
+""" Gráfico de las zonas de estabilidad de la ecuación de Mathieu. """
+
+from numba import jit
+import matplotlib.pyplot as plt
+import numpy as np
+
+res = 11
+res_lim = (res-1)//2
+
+# Término (mu - 2*k)^2 de Delta(mu). Se calcula antes por optimización
+delta_k_term = np.array([(2*k)**2  for k in range(-res_lim, res_lim+1)])
+
+# Se calcula la identidad de res*res antes por optimización
+identity = np.identity(res)
+
+
+@jit # just in time compilation
+def mu(q, p):
+
+    # Contruyo la matriz Delta(mu=0, q, p)
+    coef = q/(delta_k_term-p)
+    delta_matrix = identity + np.diag(coef[:-1], 1) + np.diag(coef[1:], -1)
+
+    # Calculo el determinante
+    det = np.linalg.det(delta_matrix)
+
+    # Si mu(q, p) es complejo retorno True; si es real, False
+    if det < 0 or det*np.sin(np.pi/2 * np.sqrt(p))**2 > 1:
+        return True
+    else:
+        return False
+
+# Vectorizo la función con resultados del tipo bool
+mu = np.vectorize(mu, otypes=[bool])
+
+# Límites de q y p, no comienza de 0 para evitar los resultados pares
+# que tienen una solución distinta (NOTE: consultar)
+qlims = [0.001, 12]
+plims = [0.001, 14]
+
+q = np.linspace(*qlims, 1_000)
+p = np.linspace(*plims, 1_000)
+qq, pp = np.meshgrid(q, p)
+
+# Calculo mu para todos los pares (q, p)
+M = mu(qq, pp)
+
+# Plot
+fig, ax = plt.subplots(figsize=(8, 9))
+im = ax.imshow(M[::-1], cmap='Blues', extent=[*qlims, *plims])
+plt.show()
---
+++ Mathieu_equations/Codigos/mathieu_equation_stability_imag.py
@@ -0,0 +1,58 @@
+""" WIP. Contour plot de la estabilidad de la ecuación de mathieu. """
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import jit
+
+
+res = 51
+res_lim = (res-1)//2
+
+def delta_coef(mu, k): return (mu-2*k)**2
+DELTA_COEF_0 = np.array([delta_coef(0, i)  for i in range(-res_lim, res_lim+1)])
+DELTA_COEF_1 = np.array([delta_coef(1, i)  for i in range(-res_lim, res_lim+1)])
+
+identity = np.identity(res)
+
+@np.vectorize
+def det_delta_0(q, p):
+    return np.linalg.det(
+                identity
+                + np.diag(q/(DELTA_COEF_0-p), 1)[:-1,:-1]
+                + np.diag(q/(DELTA_COEF_0-p), -1)[1:,1:]
+                )
+
+@np.vectorize
+def det_delta_1(q, p):
+    return np.linalg.det(
+                identity
+                + np.diag(q/(DELTA_COEF_1-p), 1)[:-1,:-1]
+                + np.diag(q/(DELTA_COEF_1-p), -1)[1:,1:]
+                )
+
+@np.vectorize
+def mu_odd(q, p):
+    return 2/np.pi * np.arcsin(np.sqrt( det_delta_0(q, p)*np.sin(np.pi/2 * np.sqrt(p))**2 ))
+
+@np.vectorize
+def mu_even(q, p):
+    return 1/np.pi * np.arccos(2*det_delta_1(q, p)-1)
+
+@np.vectorize
+def mu(q, p):
+    if np.real(p) % 2 == 0:
+        return mu_even(q, p)
+    else:
+        return mu_odd(q, p)
+
+N = 200
+q = np.linspace(0.001, 11.99, N).astype('complex')
+p = np.linspace(0.001, 13.99, N).astype('complex')
+
+pp, qq = np.meshgrid(p, q)
+M = mu(pp, qq)
+
+# Plot
+fig, ax = plt.subplots(figsize=(8, 9))
+im = ax.imshow(np.imag(M), cmap='gray', extent=[*qlims, *plims])
+plt.show()
