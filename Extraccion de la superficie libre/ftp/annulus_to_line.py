a81182381c38bf19e4447580b541cc9d6ee4d4de
Bernardo Español <esp.bernardo@gmail.com>
2021-06-30 21:59:08 -0300

    FTP: Reescritura de la creacion de los diagramas espaciotemporales

---
+++ Extraccion de la superficie libre/ftp/annulus_to_line.py
@@ -0,0 +1,128 @@
+from typing import Tuple
+import numpy as np
+from numba import jit
+
+def get_polar_band(image: np.ndarray,
+                   center: Tuple[float, float],
+                   radius_limits: Tuple[float, float],
+                   band_resolution: int):
+
+    initial_radius, final_radius = radius_limits
+
+    theta, r = np.meshgrid(np.linspace(0, 2*np.pi, band_resolution),
+                            np.arange(initial_radius, final_radius))
+
+    xcart = (r * np.cos(theta) + center[0]).astype(int)
+    ycart = (r * np.sin(theta) + center[1]).astype(int)
+
+    image_band = image[ycart, xcart].reshape(final_radius-initial_radius, band_resolution)
+    return image_band
+
+def find_annulus_region(annulus_mask, band_resolution):
+
+    half_width = annulus_mask.shape[0]//2
+    annulus_mask_band = get_polar_band(annulus_mask,
+                                       center=(half_width, half_width),
+                                       radius_limits=(0, half_width),
+                                       band_resolution=band_resolution)
+
+    initial_radius = int(half_width-np.max(annulus_mask_band.sum(0)))
+
+    final_radius = half_width
+    annulus_region_mask = annulus_mask_band[initial_radius:final_radius, :].astype(bool)
+
+    return initial_radius, final_radius, annulus_region_mask
+
+def get_polar_band_average(annulus: np.ndarray,
+                           center: Tuple[float, float],
+                           radius_limits: Tuple[float, float],
+                           annulus_region_mask: np.ndarray,
+                           band_resolution: int):
+
+    annulus_band = get_polar_band(annulus, center=center,
+                                  radius_limits=radius_limits,
+                                  band_resolution=band_resolution)
+    band_average = np.nanmean(np.where(annulus_region_mask, annulus_band, np.nan), 0)
+
+    return band_average
+
+def get_spatiotemporal_diagram(height_fields_path: str,
+                               band_resolution: int=3000):
+    """
+    Toma un HDF5 con dos grupos: uno con las imagenes ya procesadas
+    por ftp y otro con la máscara utilizada.
+
+    Retorna el diagrama espaciotemporal.
+    """
+    f = h5py.File(height_fields_path, 'r')
+    annulus_array = f['height_fields/annulus'][:, :, :]
+    annulus_mask = f['masks/annulus'][:, :, 0]
+
+    *radius_limits, annulus_region_mask = find_annulus_region(annulus_mask,
+                                                              band_resolution)
+
+    half_width = annulus_array.shape[0]//2
+    center = [half_width]*2
+
+    n_images = annulus_array.shape[-1]
+    spatiotemporal_diagram = np.zeros(shape=(n_images, band_resolution))
+    for i in range(n_images): # TODO: Pedir el annulus_array[:, :, i] de a chunks
+
+        annulus_band_average = get_polar_band_average(
+                                       annulus_array[:, :, i], center=center,
+                                       radius_limits=radius_limits,
+                                       annulus_region_mask=annulus_region_mask,
+                                       band_resolution=band_resolution)
+
+        spatiotemporal_diagram[i, :] = annulus_band_average
+
+    return spatiotemporal_diagram
+
+
+
+if __name__ == '__main__':
+    import h5py
+    import matplotlib
+    from matplotlib import pyplot as plt
+    import cProfile
+    import pstats
+
+    height_fields_path = '/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2018-07-17-0001-annulus-PRO.hdf5'
+    #  height_fields_path = '/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2019-10-09-a200-f20-fmod0-annulus-PRO.hdf5'
+    #  annulus_mask = np.asarray(f['masks/annulus'][:, :, 0])
+    #  with cProfile.Profile() as pr:
+        #  get_spatiotemporal_diagram(height_fields_path)
+
+    #  stats = pstats.Stats(pr)
+    #  stats.sort_stats(pstats.SortKey.TIME)
+    #  stats.print_stats()
+
+    spatiotemporal_diagram = get_spatiotemporal_diagram(height_fields_path)
+    plt.imshow(spatiotemporal_diagram)
+    plt.show()
+
+    #  f = h5py.File('/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2018-07-17-0001-annulus-PRO.hdf5', 'r')
+    #  f = h5py.File('/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2019-10-09-a200-f20-fmod0-annulus-PRO.hdf5', 'r')
+
+
+
+    #  annulus = f['height_fields/annulus']
+    #  annulus_mask = f['masks/annulus']
+
+    #  annulus = annulus[:, :, 40]
+    #  annulus_mask = annulus_mask[:, :, 0]
+
+    #  *radius_limits, annulus_region_mask = find_annulus_region(annulus_mask)
+
+    #  half_width = annulus.shape[0]//2
+    #  center = [half_width]*2
+    #  annulus_band_average = get_polar_band_average(
+                                   #  annulus, center=center,
+                                   #  radius_limits=radius_limits,
+                                   #  annulus_region_mask=annulus_region_mask
+                                   #  )
+
+    #  plt.plot(annulus_band_average)
+    #  #  plt.imshow(img, cmap='gray')
+    #  plt.show()
+
--- Extraccion de la superficie libre/samantha_code/BM-test.py
+++ Extraccion de la superficie libre/samantha_code/BM-test.py
@@ -1,25 +1,105 @@
 import numpy as np
 import h5py
+import matplotlib
 from matplotlib import pyplot as plt
-from unwrap import unwrap
-from modulation_instability import bulk_processing
-from modulation_instability import input_output
+#  from unwrap import unwrap
+#  from modulation_instability import bulk_processing
+#  from modulation_instability import input_output
 #  from modulation_instability import analysis
-import time
+#  import time
 
 
-destination_path = '/home/bersp/Documents/'
+med_folder = '../../Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/'
+
+processing_parameter_file = med_folder + 'processing_parameters.yaml'
+destination_path = med_folder + 'HDF5/'
+capture_parameter_file = med_folder + 'capture_parameters.yaml'
+cam_calib_path = med_folder + 'calibration/'
+acc_calib_path = med_folder + 'calibration/'
+ftp_path = med_folder
+deformed_folder = 'deformed/'
+accel_file = None
+
+
 #  input_output.create_original_dataset(
             #  destination_path=destination_path,
-            #  parameter_file='datos_para_mandar/capture_parameters.yaml',
-            #  cam_calib_path='datos_para_mandar/calibration/',
-            #  acc_calib_path='datos_para_mandar/calibration/',
-            #  ftp_path='datos_para_mandar/',
-            #  deformed_folder='deformed/',
-            #  accel_file='')
+            #  parameter_file=parameter_file,
+            #  cam_calib_path=cam_calib_path,
+            #  acc_calib_path=acc_calib_path,
+            #  ftp_path=ftp_path,
+            #  deformed_folder=deformed_folder,
+            #  accel_file=accel_file)
 
-bulk_processing.process_datafile_by_ftp(
-        destination_path=destination_path,
-        data_series_file='2018-07-17-0001-RAW.hdf5',
-        parameter_file='datos_para_mandar/processing_parameters.yaml'
-        )
+#  data_series_file = '/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2018-07-17-0001-RAW.hdf5'
+#  processing_parameter_file = med_folder + 'processing_parameters.yaml'
+
+#  bulk_processing.process_datafile_by_ftp(
+        #  destination_path=destination_path,
+        #  data_series_file=data_series_file,
+        #  parameter_file=processing_parameter_file)
+
+#  f = h5py.File('/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2018-07-17-0001-annulus-PRO.hdf5', 'r')
+f = h5py.File('/media/box/Laboratorio/Labo_6y7/Mediciones_FaradayWaves/MED - Mediciones de Samantha para testear/HDF5/2019-10-09-a200-f20-fmod0-annulus-PRO.hdf5', 'r')
+
+annulus_deformed = f['height_fields/annulus']
+annulus_mask = f['masks/annulus']
+
+#  levels = np.min(annulus_deformed[:, :, 0:20]), np.max(annulus_deformed[:, :, 0:20])
+#  levels = np.linspace(levels[0], levels[1], 300)
+#  colors = plt.get_cmap('gray')(np.linspace(0, 1, 300))[:-1]
+
+#  cmap, norm = matplotlib.colors.from_levels_and_colors(levels, colors)
+#  print(cmap)
+
+
+#  import numpy.typing as npt
+from typing import Tuple
+
+def get_polar_band(image: np.ndarray, radius_limits: Tuple[float, float]=None):
+
+    half_width = image.shape[0]//2
+
+    if radius_limits:
+        initial_radius, final_radius = radius_limits
+    else:
+        initial_radius, final_radius = 0, half_width
+
+    center = [half_width, half_width]
+
+    theta, r = np.meshgrid(np.linspace(0, 2*np.pi, 3000),
+                            np.arange(initial_radius, final_radius))
+
+    xcart = (r * np.cos(theta) + center[0]).astype(int)
+    ycart = (r * np.sin(theta) + center[1]).astype(int)
+
+    image_cart = image[ycart, xcart].reshape(final_radius-initial_radius, 3000)
+    return image_cart
+
+def find_annulus_width(annulus_mask):
+
+    annulus_maks = get_polar_band(annulus_mask)
+    initial_radius =
+    final_radius = hald_width
+
+    return initial_radius, final_radius
+
+def annulus_to_stip(annulus: np.ndarray,
+                    initial_radius: float,
+                    final_radius: float):
+
+
+    annulus = annulus
+    print(annulus.shape)
+    print(annulus_mask.astype(int).shape)
+
+    return annulus
+
+annulus_deformed = annulus_deformed[:, :, 40]
+annulus_mask = annulus_mask[:, :, 0]
+
+img = annulus_mask
+#  img = get_polar_band(img)
+
+r = np.sqrt(img.shape[0]**2 * 2)
+plt.imshow(img, cmap='gray')
+plt.show()
