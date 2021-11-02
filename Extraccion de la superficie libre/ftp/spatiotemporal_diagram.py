37cac67ef03ce3464c0abdf637d1dfc43e35fc50
Bernardo Español <esp.bernardo@gmail.com>
2021-11-02 16:31:48 -0300

    Cambio en la transformacion de cartesianas a polares

--- Extraccion de la superficie libre/ftp/spatiotemporal_diagram.py
+++ Extraccion de la superficie libre/ftp/spatiotemporal_diagram.py
@@ -10,10 +10,8 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%H:%M:%S')
 
 
-def get_polar_strip(image: np.ndarray,
-                    center: Tuple[int, int],
-                    radius_limits: Tuple[int, int],
-                    strip_resolution: int):
+def get_polar_strip(image: np.ndarray, center: Tuple[int, int],
+                    radius_limits: Tuple[int, int], strip_resolution: int):
     """
     Funcion para pasar de anillo en cartesianas a banda en coordenadas polares.
     Esta banda tiene un ancho igual a la diferencia entre dos radios límites dados.
@@ -34,18 +32,31 @@ def get_polar_strip(image: np.ndarray,
     -------
     image_strip : 2-D array
         Banda de dimensiones (strip_resolution, diff(radius_limits)).
+
+
+    Scheme
+    ------
+           0 
+          o  o          
+       o        o                           
+      o          o 3            0     1     2     3
+    1 o          o     ---->     o o o o o o o o o o o o
+       o        o   
+          o  o     
+            2                          
     """
 
     initial_radius, final_radius = radius_limits
 
-    theta, r = np.meshgrid(np.linspace(0, 2*np.pi, strip_resolution),
+    theta, r = np.meshgrid(np.linspace(0, 2 * np.pi, strip_resolution),
                            np.arange(initial_radius, final_radius))
 
-    xcart = (r * np.cos(theta) + center[0]).astype(int)
+    theta = theta - np.pi / 2
+    xcart = -(r * np.cos(theta) + center[0]).astype(int)
     ycart = (r * np.sin(theta) + center[1]).astype(int)
 
-    image_strip = image[ycart, xcart].reshape(
-        final_radius-initial_radius, strip_resolution)
+    image_strip = image[ycart, xcart].reshape(final_radius - initial_radius,
+                                              strip_resolution)
     return image_strip
 
 
@@ -63,7 +74,7 @@ def find_annulus_region(annulus_mask, center, annulus_radii, strip_resolution):
 
     """
 
-    half_width = annulus_mask.shape[0]//2
+    half_width = annulus_mask.shape[0] // 2
     annulus_mask_strip = get_polar_strip(annulus_mask,
                                          center=center,
                                          radius_limits=(0, half_width),
@@ -71,19 +82,19 @@ def find_annulus_region(annulus_mask, center, annulus_radii, strip_resolution):
 
     initial_radius, final_radius = annulus_radii
 
-    annulus_region_mask = annulus_mask_strip[initial_radius:final_radius, :].astype(
-        bool)
+    annulus_region_mask = annulus_mask_strip[
+        initial_radius:final_radius, :].astype(bool)
 
     return initial_radius, final_radius, annulus_region_mask
 
 
-def get_polar_strip_average(annulus: np.ndarray,
-                            center: Tuple[float, float],
+def get_polar_strip_average(annulus: np.ndarray, center: Tuple[float, float],
                             radius_limits: Tuple[float, float],
                             annulus_region_mask: np.ndarray,
                             strip_resolution: int):
 
-    annulus_strip = get_polar_strip(annulus, center=center,
+    annulus_strip = get_polar_strip(annulus,
+                                    center=center,
                                     radius_limits=radius_limits,
                                     strip_resolution=strip_resolution)
     masked_annulus_strip = np.where(annulus_region_mask, annulus_strip, np.nan)
@@ -112,7 +123,8 @@ def vertical_unwraping(st_diagram):
     return st_diagram
 
 
-def get_st_diagram(ftp_hdf5_path: str, transform_to_mm=True,
+def get_st_diagram(ftp_hdf5_path: str,
+                   transform_to_mm=True,
                    strip_resolution: int = 3000):
     """
     TODO: Doctring for get_st_diagram
@@ -131,48 +143,54 @@ def get_st_diagram(ftp_hdf5_path: str, transform_to_mm=True,
     center = annulus_mask_info['center']
     annulus_radii = annulus_mask_info['annulus_radii']
 
-    annulus_region_mask = get_polar_strip(annulus_mask, center, annulus_radii,
+    annulus_region_mask = get_polar_strip(annulus_mask,
+                                          center,
+                                          annulus_radii,
                                           strip_resolution=3000)
 
     n_images = annulus_array.shape[-1]
     img_per_chunk = annulus_array.chunks[-1]
 
-    n_chunks = np.ceil(n_images/img_per_chunk).astype(int)
+    n_chunks = np.ceil(n_images / img_per_chunk).astype(int)
 
     st_diagram = np.zeros(shape=(n_images, strip_resolution))
     st_error = np.zeros(shape=(n_images, strip_resolution))
 
     logging.info('ST: Inicio del cálculo del diagrama espacio-temporal')
 
-    for i in range(n_chunks-1):
+    for i in range(n_chunks - 1):
 
-        annulus_chunk = annulus_array[:, :, i *
-                                      img_per_chunk: (i+1)*img_per_chunk]
+        annulus_chunk = annulus_array[:, :, i * img_per_chunk:(i + 1) *
+                                      img_per_chunk]
         for j in range(img_per_chunk):
             annulus_strip_average, annulus_strip_std = get_polar_strip_average(
-                annulus_chunk[:, :, j], center=center,
+                annulus_chunk[:, :, j],
+                center=center,
                 radius_limits=annulus_radii,
                 annulus_region_mask=annulus_region_mask,
                 strip_resolution=strip_resolution)
 
             #  annulus_strip_average -= annulus_strip_average.mean()
-            st_diagram[i*img_per_chunk+j, :] = annulus_strip_average
-            st_error[i*img_per_chunk+j, :] = annulus_strip_std
+            st_diagram[i * img_per_chunk + j, :] = annulus_strip_average
+            st_error[i * img_per_chunk + j, :] = annulus_strip_std
 
         logging.info(f'ST: {i+1}/{n_chunks} chunks calculados')
 
     # Guardo el último chunk aparte porque podría ser más corto
-    annulus_chunk = annulus_array[:, :, (n_chunks-1)*img_per_chunk:n_images]
+    annulus_chunk = annulus_array[:, :,
+                                  (n_chunks - 1) * img_per_chunk:n_images]
     for j in range(annulus_chunk.shape[-1]):
         annulus_strip_average, annulus_strip_std = get_polar_strip_average(
-            annulus_chunk[:, :, j], center=center,
+            annulus_chunk[:, :, j],
+            center=center,
             radius_limits=annulus_radii,
             annulus_region_mask=annulus_region_mask,
             strip_resolution=strip_resolution)
 
         #  annulus_strip_average -= annulus_strip_average.mean()
-        st_diagram[(n_chunks-1)*img_per_chunk+j, :] = annulus_strip_average
-        st_error[(n_chunks-1)*img_per_chunk+j, :] = annulus_strip_std
+        st_diagram[(n_chunks - 1) * img_per_chunk +
+                   j, :] = annulus_strip_average
+        st_error[(n_chunks - 1) * img_per_chunk + j, :] = annulus_strip_std
 
     # Unwraping vertical
     logging.info(f'ST: Calculando unwraping vertical')
@@ -215,24 +233,24 @@ def create_st_hdf5(hdf5_folder):
     """
     TODO: Doctring for create_st_hdf5
     """
-    st_diagram, st_error = get_st_diagram(hdf5_folder+'FTP.hdf5')
+    st_diagram, st_error = get_st_diagram(hdf5_folder + 'FTP.hdf5')
 
-    f = h5py.File(hdf5_folder+'ST.hdf5', 'w')
+    f = h5py.File(hdf5_folder + 'ST.hdf5', 'w')
     f.create_dataset('spatiotemporal_diagram', data=st_diagram)
     f.create_dataset('spatiotemporal_diagram_error', data=st_error)
     f.close()
 
 
-if __name__ == '__main__':
+def main():
     import matplotlib.pyplot as plt
     import matplotlib
     cmap = matplotlib.cm.viridis
     med_folder = '../../Mediciones/MED39 - Subida en voltaje - 0902/'
-    hdf5_folder = med_folder+'HDF5/'
+    hdf5_folder = med_folder + 'HDF5/'
 
     create_st_hdf5(hdf5_folder)
 
-    f = h5py.File(hdf5_folder+'ST.hdf5', 'r')
+    f = h5py.File(hdf5_folder + 'ST.hdf5', 'r')
     st_diagram = np.array(f['spatiotemporal_diagram'])
     st_error = np.array(f['spatiotemporal_diagram_error'])
 
@@ -244,3 +262,7 @@ if __name__ == '__main__':
     plt.imshow(st_diagram)
     plt.colorbar()
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
